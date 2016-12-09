82dba38bfc1e69fcdf7594b6228614a399ebb95a
Brian Liou <brian.liou91@gmail.com>
2016-12-09 12:06:14 -0500

    Removed friendship controller and moved actions into userscontroller, fix ajax requests on front end

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,8 +12,30 @@ class Api::UsersController < ApplicationController
 
   end
 
-  def add_friend
+  def addfriend
+    friend_email = user_params[:email]
+    @friend = User.find_by(email: friend_email)
 
+    if @friend.nil?
+      render json: ['User does not exist!'], status: 401
+    elsif @friend.id == current_user.id
+      render json: ['You cannot friend yourself'], status: 401
+    else
+      @friendship = Friendship.new(user_id:current_user.id, friend_id: @friend.id)
+
+      if @friendship.save
+        render json: @friend
+      else
+        render json: ['Invalid user, user does not exist'], status: 401
+      end
+    end
+
+  end
+
+  def showfriends
+    user = User.find(current_user.id)
+    @friends = user.friends
+    render :friends
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
+    resources :users, only: [:create] do
+      collection do
+        post 'addfriend'
+        get 'showfriends'
+      end
+    end
     resource :session, only: [:create, :destroy]
     resources :friendships, only: [:create, :destroy, :index, :show]
   end
