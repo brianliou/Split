e8d1c30181f38d661cb954279cb25a4026aa7874
Brian Liou <brian.liou91@gmail.com>
2016-12-16 09:55:59 -0500

    Remove old code

--- frontend/reducers/bill_reducer.js
+++ frontend/reducers/bill_reducer.js
@@ -13,17 +13,6 @@ const billReducer = (state = initialState, action) => {
   let tempState;
   Object.freeze(state);
   switch(action.type) {
-    // case RECEIVE_BILL:
-    //   // if you've created a bill you need to add it to your list of bills
-    //   //
-    //   // return a new state of bills with the new bill added
-    //   newState.update = !newState.update;
-    //   // newState.billList = action.bill;
-    //   debugger
-    //   return newState;
-    // case RECEIVE_BILL_SETTLE:
-    //   newState.billList = action.bills
-    //   return newState;
     case RECEIVE_ERRORS:
       newState.errors = action.errors;
       return newState;
